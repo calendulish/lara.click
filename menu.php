41d22bf002ad0f77bd7aab0a1a4a81f6914de3cc
Lara Maia <dev@lara.click>
2015-11-06 00:50:38 -0200

    Remove webmail from menu

--- menu.php
+++ menu.php
@@ -5,8 +5,6 @@
             <li class="divider-vertical"></li>
             <li><a href="?page=downloads">Downloads</a><img alt="downloads" src="icons/download.svg" /></li>
             <li class="divider-vertical"></li>
-            <li><a href="?page=mail">Webmail</a><img alt="mail" src="icons/mail.svg" /></li>
-            <li class="divider-vertical"></li>
             <li><a href="?page=contact">Contact</a><img alt="contact" src="icons/contact.svg" /></li>
             <li class="divider-vertical"></li>
         </ul>
