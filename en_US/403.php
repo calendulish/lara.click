2a28010969b733dc75cd0e5a3acd6fa20aaf8880
Lara Maia <dev@lara.click>
2015-12-07 13:46:40 -0200

    Fix error pages

---
+++ en_US/403.php
@@ -0,0 +1,2 @@
+<h1>403</h1>
+<p>You don't have permission to access the requested page or file.</p>
---
+++ pt_BR/403.php
@@ -0,0 +1,2 @@
+<h1>403</h1>
+<p>Você não tem permissão para acessar a página ou arquivo requisitado.</p>
