7b4fa37577012ee6303e48f261a3ef06ba6627b2
Lara Maia <dev@lara.click>
2016-01-07 15:23:04 -0200

    [2/3] Add comments section

--- pt_BR/tecblog.php
+++ pt_BR/tecblog.php
@@ -1,6 +1,8 @@
 <h1>TecBlog</h1>
 <h3>Descobertas interessantes sobre tecnologia</h1>
 <?php
+require_once('comments.php');
+
 if(!isset($_GET['tecblogpg'])||$_GET['tecblogpg'] == ""||$_GET['tecblogpg'] == 0) {
     $blogpg = 1;
 } else {
@@ -9,7 +11,7 @@ if(!isset($_GET['tecblogpg'])||$_GET['tecblogpg'] == ""||$_GET['tecblogpg'] == 0
 
 $posts_per_page = 5;
 
-$ignores = array('.', '..', 'media', 'index.php');
+$ignores = array('.', '..', 'comments', 'media', 'index.php');
 $files = array_values(array_diff(scandir('tecblog/pt_BR', 1), $ignores));
 
 for($i = $posts_per_page*$blogpg-$posts_per_page; $i < $posts_per_page*$blogpg; $i++) {
@@ -21,8 +23,29 @@ for($i = $posts_per_page*$blogpg-$posts_per_page; $i < $posts_per_page*$blogpg; 
     print('<h2 class="title">'.$title."</h2>\n");
     print("<div class='postcontents'>\n");
     include('tecblog/pt_BR/'.$file);
-    print("<p class='signature'><i>Lara Maia</i></p>\n</div>\n</div>");
+    print("<p class='signature'><i>Lara Maia</i></p>\n</div>\n");
 }
+?>
+<div class="commentsForm">
+    <form class="commentsFormAlign" action="" method="POST">
+        <p>Nome: <input id='focus' class="commentsFormName" type="text" name="name" required>
+        <textarea class="commentsFormText" name="message" rows="6" cols="40"
+        placeholder="Escreva seu comentário aqui." required></textarea></p>
+        <input class="commentsFormSubmit" type="submit" name="comment" value="Enviar">
+    </form>
+<?php
+    $commentsFile = 'tecblog/pt_BR/comments/'.$title.'.json';
+    if (isset($_POST['comment'])) {
+        if(writeComment($commentsFile) == 1) {
+            print("<p class='commentdup'>Comentário duplicado. Não será publicado.</p>");
+        }
+    }
+    if(file_exists($commentsFile)) {
+        readComment($commentsFile);
+    }
+
+    print("</div>\n</div>\n");
+
 if($blogpg > 1) {
 printf("<a href='?page=tecblog&tecblogpg=%d'>Anterior</a>\n", $blogpg-1);
 }
