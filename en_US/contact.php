677396bc767871905268bee6b18ba8827e2076ac
Lara Maia <dev@lara.click>
2017-08-10 10:37:57 -0300

    Use external smtp server for send mail

--- en_US/contact.php
+++ en_US/contact.php
@@ -1,15 +1,35 @@
 <?php
 if(isset($_POST['contact'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $formcontent="From: $name\nMessage: $message";
-    $recipient = "dev@lara.click";
-    $subject = "Contact from lara.click";
-    $mailheader = "From: $email \r\n";
-    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-    echo "Thank You!";
-} else {
+    function mail_callback($errno, $errstr) {
+        echo "<h2>Fatal error!</h2>\n";
+        echo '<p>Error ' . $errno . ': ' . substr(strrchr($errstr, 'mail.php):'), 2), "</p>\n";
+        echo "<h4>Please, contact us at dev@lara.click</h4>\n";
+        exit(1);
+    }
+
+    $mail_script = 'http://lara.click/mail.php';
+
+    $options = array(
+        'http' => array(
+                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method' => 'POST',
+                'content' => http_build_query($_POST),
+            )
+        );
+
+    $context = stream_context_create($options);
+
+    set_error_handler('mail_callback', E_WARNING);
+    $response = file_get_contents($mail_script, false, $context);
+    restore_error_handler();
+
+    if($response == 'sent') {
+        echo "<h3>Message has been sent.<br/>\nThank you</h3>";
+    } else {
+        echo "<h3>Message could not be sent.</h3>\n";
+        echo "<p>Error:" . $response . "</p>\n";
+    }
+}
 ?>
 
 <form method="POST">
@@ -20,7 +40,3 @@ if(isset($_POST['contact'])) {
     <br />
     <input type="submit" name="contact" value="Send"> <input type="reset" value="Clear">
 </form>
-
-<?php
-}
-?>
