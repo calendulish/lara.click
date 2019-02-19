994827d73212f1f1f162cb9a49807743a20fff4c
Lara Maia <dev@lara.click>
2019-02-19 06:52:14 -0300

    remove animation, fix includes

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
+require_once("config.php");
+// require_once($_SESSION['lang']."/config.php");
+require_once("cute-php-explorer/init.php");
+
 session_start();
 
 $userlang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
 if(isset($_POST['lang'])) {
     $_SESSION['lang'] = $_POST['lang'];
 } else {
@@ -21,10 +26,6 @@ if(isset($_POST['lang'])) {
 
 if(isset($_GET['page'])) {
     if(file_exists($_SESSION['lang'].'/'.$_GET['page'].'.php')) {
-        if($_GET['page'] == "downloads") {
-            require_once("config.php");
-            require_once("cute-php-explorer/init.php");
-        }
         $page = $_SESSION['lang'].'/'.$_GET['page'];
     } else {
         header("HTTP/1.0 404 Not Found");
@@ -41,18 +42,11 @@ if(isset($_GET['page'])) {
     <title>lara.click - HOME</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
-    <?php if($page == $_SESSION['lang']."/downloads") { include_once("cute-php-explorer/head.php"); } ?>
+    <?php include_once("cute-php-explorer/head.php") ?>
     <link href='http://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
     <link href="style.css" rel="stylesheet">
     <link href="data:image/x-icon;base64,AAABAAEAEBAQAAAAAAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAACESfcAnCv8ANOf/wC8cP8A////AOC5/wDJ//8A8Nn/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAREREREREREREREQzQzMEREREQ3ISUmBEREQ3IFVyMEREQ3IDMwMwREQzMCcnIARERDQCd3J3UERDRAJydzIgREREA3cnI2BEREQDVzMzMERERAUHNmMEREREBQU2YwREREQGUDMwRERERAIgNjBEREREQAQABERERERERERERET//wAA/IcAAPgDAADwAwAA4AMAAMAHAADQAwAAsAMAAPADAADwAwAA8AcAAPAHAADwDwAA8A8AAPkfAAD//wAA" rel="icon" type="image/x-icon" />
     <script>
-    function ani(link) {
-        aniElement = document.getElementById("animation")
-        aniElement.style.opacity = "1";
-        aniElement.className = "outAnim contents";
-        setTimeout(function(){window.location=link;}, 500)
-    }
-
     function changeclass(className, value){
         e = document.getElementsByClassName(className);
         while(e.length > 0) e[0].className = value;
@@ -87,14 +81,17 @@ if(isset($_GET['page'])) {
     window.addEventListener('load', function() {
         check_menu_size();
         window.addEventListener('resize', check_menu_size);
-
-        menuElement = document.getElementById("menu")
-        menuElement.style.opacity = "1";
-
-        aniElement = document.getElementById("animation")
-        aniElement.style.opacity = "0";
-        aniElement.className = "inAnim contents";
     });
+    </script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-65630368-1', 'auto');
+        ga('send', 'pageview');
+
     </script>
 </head>
 
