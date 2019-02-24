fd0f33ba93144175acccc3e656983c78b124f61c
Lara Maia <dev@lara.click>
2019-02-24 05:23:28 -0300

    Update footer

--- footer.php
+++ footer.php
@@ -6,13 +6,15 @@
         <img alt="Built with love" src="icons/built_with_love.svg" width="150"/>
     </a>
 
-    <p>The lara.click website is free software: you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    See the GNU General Public License for more details:
+    <p>
+<?php
+    print(_("The lara.click website is free software: you can redistribute it and/or\n"));
+    print(_("modify it under the terms of the GNU General Public License as published\n"));
+    print(_("by the Free Software Foundation, either version 3 of the License, or\n"));
+    print(_("(at your option) any later version.\n\n"));
+    print(_("See the GNU General Public License for more details:\n"));
+?>
     <a href=http://www.gnu.org/licenses/>http://www.gnu.org/licenses/</a>.
 
-    Lara Maia © 2015~2016 <a href="mailto:dev@lara.click">&lt;dev@lara.click&gt;</a>
-    GPG Fingerprint: 9626 A58D 0AC4 F421 8F56  E7EA 1D0D 82CC 49BB D383</p>
+    Lara Maia © 2015~2019 <a href="mailto:dev@lara.click">&lt;dev@lara.click&gt;</a>
+    GPG Fingerprint: 48F4 C8D8 CF52 1F4A 46E2  BA49 C879 9F9E A7AF 292D</p>
