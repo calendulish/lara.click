15fde22df54ffe42630ef7d93345c51fe9aaa6a2
Lara Maia <dev@lara.click>
2019-02-27 09:21:16 -0300

    gtk: Fix spaces

--- gtk.php
+++ gtk.php
@@ -13,21 +13,21 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Lara Maia <dev@lara.click> © 2019
+ * Lara Maia <dev@lara.click> (C) 2019
  *
  * DO NOT USE THAT! IT'S UGLY! IT'S RUDE! THANKS!
- 
+
  * but it's cool...
  */
- 
- class Widget {
+
+class Widget {
     private $expand = false;
     private $homogeneous = true;
-    
+
     public function get_html() {
         return null;
     }
-    
+
     public function set_params(&$widget) {
         if($this->expand) {
             $widget .= " expand";
@@ -35,15 +35,15 @@
             $widget .= " homogeneous";
         }
     }
-    
+
     public function set_expand($expand) {
         $this->expand = $expand;
     }
-    
+
     public function set_homogeneous($homogeneous) {
         $this->homogeneous = $homogeneous;
     }
-    
+
     public function show() {
         print($this->get_html());
     }
@@ -70,14 +70,14 @@ class Window extends Widget {
         $this->expand = $expand;
         $this->homogeneous = $homogeneous;
     }
-    
+
     public function get_html() {
         parent::set_params($this->window);
-        
+
         if(!empty($this->title)) {
             $this->window .= "' id='" . $this->title;
         }
-        
+
         $this->window .= "'>\n";
 
         if(!empty($this->hyperlink)) {
@@ -103,7 +103,7 @@ class Window extends Widget {
 
         $close_button = null;
         $minimize_button = null;
-            
+
         if(!empty($this->close_button_callback)) {
             $close_button = "<button class='close_button' onclick='" . $this->close_button_callback . '(';
 
@@ -121,10 +121,10 @@ class Window extends Widget {
 
             $close_button .= ");'>X</button>";
         }
-        
+
         if(!empty($this->minimize_button_callback)) {
             $minimize_button = "<button class='minimize_button' onclick='" . $this->minimize_button_callback . '(';
-            
+
             foreach($this->minimize_button_params as $param) {
                 if(is_array($param)) {
                     $minimize_button .= '[';
@@ -136,10 +136,10 @@ class Window extends Widget {
                     $minimize_button .= '"' . $param . '",';
                 }
             }
-            
+
             $minimize_button .= ");'>_</button>";
         }
-        
+
         return $this->window . $this->headerbar . $minimize_button . $close_button . $this->icon;
     }
 
@@ -147,37 +147,37 @@ class Window extends Widget {
         $this->close_button_callback = $close_button_callback;
         $this->close_button_params = $close_button_params;
     }
-    
+
     public function set_minimize_button($minimize_button_callback, $minimize_button_params) {
         $this->minimize_button_callback = $minimize_button_callback;
         $this->minimize_button_params = $minimize_button_params;
     }
 
     public function set_title($title) {
-        $this->title = $title;    
+        $this->title = $title;
     }
-    
+
     public function set_icon($icon) {
         $this->icon_image = $icon;
     }
-    
+
     public function set_hyperlink($hyperlink) {
         $this->hyperlink = $hyperlink;
     }
-    
+
     public function add(&$widget) {
         $this->extra .= $widget->get_html();
     }
-    
+
     public function add_raw($contents) {
         $this->extra .= $contents;
     }
-    
+
     public function show() {
         parent::show();
         print("</div>\n");
     }
-    
+
     public function show_all() {
         parent::show();
         print($this->extra);
@@ -187,23 +187,23 @@ class Window extends Widget {
 
 class Entry extends Widget {
     protected $entry = "<div class='entry";
-    
+
     private $text;
-    
+
     public function __construct($text = null) {
         $this->text = $text;
     }
-    
+
     public function get_html() {
         parent::set_params($this->entry);
         $this->entry .= "'>";
         return $this->entry . "\n<p>\n" . $this->text . "\n</p>\n</div>\n";
     }
-    
+
     public function set_text($text) {
         $this->text = wordwrap(str_replace("\n", ' ', $text));
     }
-    
+
     public function get_text() {
         return $this->text;
     }
@@ -211,77 +211,77 @@ class Entry extends Widget {
 
 class Image extends Widget {
     protected $image = "<div class='image";
-    
+
     private $image_file;
     private $title;
     private $hyperlink;
-    
+
     public function __construct($image = null, $title = null, $hyperlink = null) {
         $this->image_file = $image;
         $this->title = $title;
         $this->hyperlink = $hyperlink;
     }
-    
+
     public function get_html() {
         parent::set_params($this->image);
         $this->image .= "'>\n<a";
-        
+
         if(!empty($this->hyperlink)) {
             $this->image .= " href='" . $this->hyperlink . "'";
         }
-        
+
         $this->image .= ">";
-        
+
         if(!empty($this->image_file)) {
             $this->image .= "<img title='" . $this->title . "' src='" . $this->image_file . "'/>";
         }
-     
+
         $this->image .= "</a></div>\n";
-        
+
         return $this->image;
     }
-    
+
     public function set_from_file($image) {
         $this->image_file = $image;
     }
-    
+
     public function set_hyperlink($hyperlink) {
         $this->hyperlink = $hyperlink;
     }
-    
+
     public function set_title($title) {
-        $this->title = $title;    
+        $this->title = $title;
     }
 }
 
 class Label extends Widget {
     protected $label = "<div class='label";
-    
+
     private $text;
     private $tag;
-    
+
     public function __construct($text = null, $tag = 'p') {
         $this->text = $text;
         $this->tag = $tag;
     }
-    
+
     public function get_html() {
         parent::set_params($this->label);
         $this->label .= "'>";
-        
+
         if(!empty($this->text)) {
             $this->label .= "<" . $this->tag . ">" . $this->text . "</" . $this->tag . ">";
         }
-        
+
         $this->label .= "</div>";
-        
+
         return $this->label;
     }
-    
+
     public function set_text($text) {
         $this->text = $text;
     }
-    
+
     public function set_tag($tag) {
         $this->tag = $tag;
     }
@@ -289,36 +289,36 @@ class Label extends Widget {
 
 class ListStore extends widget {
     protected $treeview = "<ul class='liststore";
-    
+
     private $items = [];
     private $title;
-    
+
     public function __construct($items = [], $title = null) {
         $this->items = $items;
         $this->title = $title;
     }
-    
+
     public function get_html() {
         parent::set_params($this->treeview);
         $this->treeview .= "'>";
-        
+
         if(!empty($this->title)) {
             $this->treeview .= "<lh><p>" . $this->title . "</p></lh>";
         }
-        
+
         foreach($this->items as &$item) {
             $this->treeview .= "<li><p>" . $item . "</p></li>";
         }
-        
+
         $this->treeview .= "</ul>";
-        
+
         return $this->treeview;
     }
-    
+
     public function add_item($item) {
         array_push($this->items, $item);
     }
-    
+
     public function set_title($title) {
         $this->title = $title;
     }
@@ -371,40 +371,40 @@ class TreeStore extends Widget {
 
 class Button extends Widget {
     protected $button = "<button class='button";
-    
+
     private $contents;
     private $function_;
     private $params_;
-    
+
     public function __construct($contents = null, $function_ = null, $params_ = []) {
         $this->contents = $contents;
         $this->function_ = $function_;
         $this->params_ = $params_;
     }
-    
+
     public function get_html() {
         parent::set_params($this->button);
-        
+
         if(!empty($this->function_)) {
             $this->button .= "' onclick='" . $this->function_ . '(';
-            
+
             foreach($this->params_ as $param) {
                 $this->button .= '"' . $param . '",';
             }
 
             $this->button .= ');';
         }
-        
+
         $this->button .= "'>";
-        
+
         return $this->button . $this->contents . "</button>";
     }
-    
+
     public function connect($function_, $params_) {
         $this->function_ = $function_;
         $this->params_ = $params_;
     }
-    
+
     public function set_contents($contents) {
         $this->contents = $contents;
     }
