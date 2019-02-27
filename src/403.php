b595f6565ba843c146b0f06ce759615b66864c26
Lara Maia <dev@lara.click>
2019-02-27 14:53:25 -0300

    Update error pages

--- src/403.php
+++ src/403.php
@@ -1,2 +1,29 @@
-<h1>403</h1>
-<p>You don't have permission to access the requested page or file.</p>
+<?php
+require_once('src/gtk.php');
+
+$error = new Window();
+$error->set_expand(True);
+
+$title = new Label();
+$title->set_expand(True);
+$title->set_text('404');
+$title->set_tag('h1');
+$error->add($title);
+
+$contents = new Label();
+$contents->set_expand(True);
+
+if($_SESSION['lang'] == 'en_US') {
+    $contents->set_text('You dont have permission to access the requested page or file.');
+} else {
+    $contents->set_text('Você não tem permissão para acessar a página ou arquivo requisitado.');
+}
+
+$error->add($contents);
+
+$home = new Button();
+$home->connect('home');
+$home->set_contents(_('Back'));
+$error->add($home);
+
+$error->show_all();
--- src/404.php
+++ src/404.php
@@ -1,6 +1,38 @@
-<h1>404</h1>
-<p>The page that you tried to access does not exist. If you got here from an external link<br/>
-please, notify the admin of the site that he is linking a broken link. If you got here from<br/>
-a link on google.com, please notify google. If you typed this adress on your own, try going<br/>
-to the front of lara.click instead and navigating using the menu, I created for this purpose.<br/>
-if the menu don't work, it's because i'm too stupid, just ignore it.</p>
+<?php
+require_once('src/gtk.php');
+
+$error = new Window();
+$error->set_expand(True);
+
+$title = new Label();
+$title->set_expand(True);
+$title->set_text('404');
+$title->set_tag('h1');
+$error->add($title);
+
+$contents = new Label();
+$contents->set_expand(True);
+
+if($_SESSION['lang'] == 'en_US') {
+    $contents->set_text('The page that you tried to access does not exist. If you got here from an external link
+    please, notify the admin of the site that he is linking a broken link. If you got here from
+    a link on google.com, please notify google. If you typed this adress on your own, try going
+    to the front of lara.click instead and navigating using the menu, I created for this purpose.
+    if the menu dont work, its because im too stupid, just ignore it.');
+} else {
+    $contents->set_text('A página que você tentou acessar não existe. Se você chegou aqui através de um
+    link externo por favor, notifique o administrador do site que ele está linkando um
+    link quebrado. Se você veio aqui através de um link do google.com, por favor notifique
+    o google. Se foi você mesmo que escreveu esse endereço, tente ir para o início do
+    lara.click em vez disso e navegue usando o menu, eu o criei para esse propósito.
+    Se o menu não funcionar, isso é porque eu sou muito estúpida, somente ignore.');
+}
+
+$error->add($contents);
+
+$home = new Button();
+$home->connect('home');
+$home->set_contents(_('Back'));
+$error->add($home);
+
+$error->show_all();
