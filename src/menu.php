85946d490bda31057cfbc74f30827ced98a8ccf5
Lara Maia <dev@lara.click>
2019-02-27 15:01:40 -0300

    Update menu

--- src/menu.php
+++ src/menu.php
@@ -1,20 +1,55 @@
-    <nav class='menu' id='menu'>
-        <ul class='menu'>
-            <li class='menu'>
-                <button class='menu' onclick="toggle_dropdown('start_menu')"><?php print(_('Start')) ?></button>
-                <div class="menu_content" id="start_menu">
-                    <a class="menu" href="#"><img class="menu" src="icons/contact.svg" width="30"/><?php print(_('About')) ?></a>
-                </div>
-            </li>
-            <li class='menu right'>
-                <div class="clock"></div>
-            </li>
-            <li class='menu right'>
-                <button class='menu' onclick="toggle_dropdown('language_menu')"><?php print(_('Language')) ?></button>
-                <div class="menu_content right" id="language_menu">
-                    <a href='<?= $CuteExplorer->make_query('lang', 'pt_BR', 'post') ?>'><?= 'pt_BR ' . _('(Portuguese)') ?></a>
-                    <a href='<?= $CuteExplorer->make_query('lang', 'en_US', 'post') ?>'><?= 'en_US ' . _('(English)') ?></a>
-                </div>
-            </li>
-        </ul>
-    </nav>
+<?php
+print("<nav class='menu'>");
+
+if(isset($_SESSION['mobile']) && $_SESSION['mobile'] == "on") {
+    $start_menu = new Menu('start', _('☰'));
+    $start_menu->set_align('right');
+
+    $start_menu->add_item(null, _('Home'), parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    $start_menu->add_item(null, 'pt_BR ' . _('(Portuguese)'), $CuteExplorer->make_query('lang', 'pt_BR'));
+    $start_menu->add_item(null, 'en_US ' . _('(English)'), $CuteExplorer->make_query('lang', 'en_US'));
+
+    if(isset($_SESSION['overlay']) && $_SESSION['overlay'] == 'off') {
+        $start_menu->add_item(null, _("Tablet Mode On"), $CuteExplorer->make_query('overlay', 'on'));
+    } else {
+        $start_menu->add_item(null, _("Tablet Mode Off"), $CuteExplorer->make_query('overlay', 'off'));
+    }
+
+    $start_menu->show_all();
+} else {
+    $start_menu = new Menu('start', _('Start'));
+    $start_menu->add_item('icons/contact.svg', _('About'), '#');
+
+    $start_menu->show_all();
+
+    print("<div class='clock right'></div>\n");
+
+    $language_menu = new Menu('language', _('Language'));
+    $language_menu->set_align('right');
+    $language_menu->add_item(null, 'pt_BR ' . _('(Portuguese)'), $CuteExplorer->make_query('lang', 'pt_BR'));
+    $language_menu->add_item(null, 'en_US ' . _('(English)'), $CuteExplorer->make_query('lang', 'en_US'));
+    $language_menu->show_all();
+
+    $disable_overlay_menu = new Menu('disable_overlay', _("Power"));
+    $disable_overlay_menu->set_align('right');
+
+    if(isset($_SESSION['overlay']) && $_SESSION['overlay'] == 'off') {
+        $disable_overlay_menu->add_item(null, _("Tablet Mode On"), $CuteExplorer->make_query('overlay', 'on'));
+    } else {
+        $disable_overlay_menu->add_item(null, _("Tablet Mode Off"), $CuteExplorer->make_query('overlay', 'off'));
+    }
+
+    $disable_overlay_menu->show_all();
+}
+
+
+
+if(isset($_GET['program'])) {
+    $taskbar = new Menu('taskbar', $_GET['program'], 'location.href="' . $CuteExplorer->make_query('task', $_GET['program'], ['program']) . '"');
+    $taskbar->show_all();
+} elseif(isset($_GET['task'])) {
+    $taskbar = new Menu('taskbar', $_GET['task'], 'location.href="' . $CuteExplorer->make_query('program', $_GET['task'], ['task']) . '"');
+    $taskbar->show_all();
+}
+
+print("</nav>\n");
